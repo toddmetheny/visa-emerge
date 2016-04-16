38413d57dc8d53efd2dd2a79cfad286de18c9472
Alain Fontaine <criticerz@gmail.com>
2016-04-16 11:16:15 -0400

    first commit bro

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Visapay::Application.config.secret_token = '919eb1d1797490f1f3ffa3a5cacbc4d46670841071b7e25226bba267c7ed04427cb0b7ffba827838247b40855f8b0034dcd55b80affbb69acef840610ad64303'
