7587388dc92b632cd38f398102d98d4a8af7d3c1
Jose Casanova <josescasanova@gmail.com>
2016-04-16 17:09:49 -0400

    commit

--- app/models/wit_wrapper.rb
+++ app/models/wit_wrapper.rb
@@ -13,17 +13,26 @@ class WitWrapper < ActiveRecord::Base
 
     actions = {
       :say => -> (session_id, context, msg) {
-        p '----msg----'
-        p msg
+        # p '----msg----'
+        # p msg
       },
       :merge => -> (session_id, context, entities, msg) {
-        p '----context----'
-        puts context
+        # p '----context----'
+        # puts context
         amount = first_entity_value entities, 'amount_of_money'
         context['amount'] = amount unless amount.nil?
 
         contact = first_entity_value entities, 'contact'
         context['contact'] = contact unless contact.nil?
+
+        creditcard_number = first_entity_value entities, 'creditcard_number'
+        context['creditcard_number'] = creditcard_number unless creditcard_number.nil?
+
+        expiration_date = first_entity_value entities, 'datetime'
+        context['expiration_date'] = expiration_date unless expiration_date.nil?
+
+        security_number = first_entity_value entities, 'number'
+        context['security_number'] = security_number unless security_number.nil?
 
         return context
       },
@@ -35,27 +44,29 @@ class WitWrapper < ActiveRecord::Base
         puts "Sending #{context['contact']} $#{context['amount']}"
         return context
       },
+      :storeCC => -> (session_id, context) {
+        puts "Storing CC #{context['creditcard_number']} #{context['expiration_date']} #{context['security_number']}"
+        return context
+      },
+      :transactionHistory => -> (session_id, context) {
+        puts "Transaction history"
+        return context
+      },
     }
     client = Wit.new access_token, actions
 
     session_id = 'my-user-id-42'
-    response = client.run_actions session_id, 'Send @joe $5', {}
 
-    p '----response----'
-    p response
+    puts 'What is your credit card number?'
+    response_1 = client.run_actions session_id, '4242424242424242', {}
+    response_2 = client.run_actions session_id, 'september 2020', response_1
+    response_3 = client.run_actions session_id, '2033', response_2
 
-    # session_id = 'my-user-id-42'
-    # resp = client.message 'Send @todd $5!'
-    # return resp
-    # p "Yay, got Wit.ai response: #{resp}"
-    # client.run_actions session_id, 'Send @todd $5!', {}
+    puts '-------cc response-----'
+    p response_3
 
-    # resp = client.converse 'my-user-session-42', 'Send @todd $5', {}
-    # p "Yay, got Wit.ai response: #{resp}"
+    response_1 = client.run_actions session_id, 'What is my transaction history?', {}
+    p response_1
 
-    # session = 'my-user-session-42'
-    # context0 = {}
-    # context1 = client.run_actions session, 'Send @todd $5', context0
-    # p "The session state is now: #{context1}"
   end
 end
